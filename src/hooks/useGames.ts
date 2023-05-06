73c05f7dbbc02fcc5026d2be3a6e268f90eeee1f
Felix-Antoine <felixalavoie@gmail.com>
2023-05-06 15:20:10 -0400

    adding list of platforms as text list

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,21 @@ import React, { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+interface Export {
+  id: number;
+}
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
