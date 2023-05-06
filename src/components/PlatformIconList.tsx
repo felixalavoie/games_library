66f0e1b76a45c9bc3590d090530e18feba30f656
Felix-Antoine <felixalavoie@gmail.com>
2023-05-06 15:33:35 -0400

    replacing platform names with icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,5 +1,16 @@
-import React from "react";
-import { Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneAndroid, MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon, Text } from "@chakra-ui/react";
 import { Platform } from "../hooks/useGames";
 
 interface Props {
@@ -7,12 +18,24 @@ interface Props {
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    android: FaAndroid,
+    linux: FaLinux,
+    ios: MdPhoneAndroid,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+  };
+
   return (
-    <>
+    <HStack marginTop={"30px"}>
       {platforms.map((platform) => (
-        <Text key={platform.id}>{platform.name}</Text>
+        <Icon as={iconMap[platform.slug]} key={platform.id} color="gray.500" />
       ))}
-    </>
+    </HStack>
   );
 };
 
